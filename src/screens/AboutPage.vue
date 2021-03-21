6b98fcbd8bb503423e3b34882000cb53e70fa4c3
Hasini <hasinisama99@gmail.com>
2021-03-21 07:52:00 +0530

    About Page Layout Added

--- src/screens/AboutPage.vue
+++ src/screens/AboutPage.vue
@@ -1,5 +1,79 @@
 <template>
-  <h1>About</h1>
+  <div>
+    <section>
+      <div class="container-fluid p-3 p-md-5">
+        <div class="row">
+          <div class="col-md-5">
+            <b-card style="border-radius:7px;">
+              <img src="../assets/images/Mango.jpg" alt="Mango Tree" class="rounded">
+            </b-card>
+          </div>
+          <div class="col-md-7 pl-0 pl-md-5">
+            <div class="about">
+              <h1 class ="center">About Us</h1>
+              <hr>
+              <h3 class = "text-green">We aspire to make home gardening a common practice in our country by giving a place for it to prosper.</h3>
+              <br>
+              <h3 class = "text-green">We wish to serve both home gardeners who are looking for a way to earn an income from their hard work and the customers who want a convenient and easy place to buy fresh organic produce.</h3>
+            </div>  
+          </div>
+          <div class="row form-group">
+          </div>
+          <div class="row form-group">
+          </div>    
+        </div>
+        <div class="row">
+          <div class="col-md-7">
+            <div class="story">
+              <h1>Our Story</h1>
+              <hr>
+              <pre>We aspire to make home gardening a common practice in our country</pre>
+            </div>  
+          </div>
+          <div class="col-md-5 pl-0 pl-md-5 center">
+            <img src="../assets/images/Mango.jpg" alt="Mango Tree" class="rounded-circle">  
+          </div>
+          <div class="row form-group">
+          </div>
+          <div class="row form-group">
+          </div>      
+        </div>
+        <div class="row">
+          <div class="col-md-5">
+            <b-card style="border-radius:7px;">
+              <img src="../assets/images/Mango.jpg" alt="Mango Tree" class="rounded">
+            </b-card>
+          </div>
+          <div class="col-md-7 pl-0 pl-md-5">
+            <div class="team">
+              <h1>Our Team</h1>
+              <hr>
+              <pre>We aspire to make home gardening a common practice in our country</pre>
+            </div>  
+          </div>
+          <div class="row form-group">
+          </div>    
+        </div>        
+        <div class="row mt-2 mt-md-4">
+          <h2>Contact Details</h2>
+          <div class="contact-details">
+            <table>
+              <tr>
+                <td class=""><i class="fas fa-phone-alt"></i></td>
+                <td class="px-2"><strong>&nbsp;Phone: </strong></td>
+                <td><span>+94789654563</span></td>
+              </tr>
+              <tr>
+                <td class=""><i class="fas fa-map-marker-alt"></i></td>
+                <td class="px-2"><strong>&nbsp;Location: </strong></td>
+                <td><span>Dambulla</span></td>
+              </tr>
+            </table>
+			</div>    
+        </div>    
+      </div>
+    </section>
+  </div>  
 </template>
 
 <script>
