e1270e2046aae37083718c4e11bf612c310502a1
Hasini <hasinisama99@gmail.com>
2021-03-21 11:57:00 +0530

    Updated layout

--- src/screens/AboutPage.vue
+++ src/screens/AboutPage.vue
@@ -3,19 +3,17 @@
     <section>
       <div class="container-fluid p-3 p-md-5">
         <div class="row">
-          <div class="col-md-5">
-            <b-card style="border-radius:7px;">
-              <img src="../assets/images/Mango.jpg" alt="Mango Tree" class="rounded">
-            </b-card>
-          </div>
           <div class="col-md-7 pl-0 pl-md-5">
             <div class="about">
-              <h1 class ="center">About Us</h1>
+              <h1 class="display-2">About Us</h1>
               <hr>
-              <h3 class = "text-green">We aspire to make home gardening a common practice in our country by giving a place for it to prosper.</h3>
+              <h3 class = "my-font text-green">We aspire to make home gardening a common practice in our country by giving a place for it to prosper.</h3>
               <br>
-              <h3 class = "text-green">We wish to serve both home gardeners who are looking for a way to earn an income from their hard work and the customers who want a convenient and easy place to buy fresh organic produce.</h3>
+              <h3 class = "my-font text-green">We wish to serve both home gardeners who are looking for a way to earn an income from their hard work and the customers who want a convenient and easy place to buy fresh organic produce.</h3>
             </div>  
+          </div>
+          <div class="col-md-5 pl-0 pl-md-5 text-center">
+            <img src="../assets/images/mango.jpeg" alt="Mango Tree" class="rounded-circle">  
           </div>
           <div class="row form-group">
           </div>
@@ -23,15 +21,18 @@
           </div>    
         </div>
         <div class="row">
+          <div class="col-md-5">
+            <b-card style="border-radius:7px;">
+              <img src="../assets/images/vegetable.jpeg" alt="Vegetables" class="rounded">
+            </b-card>
+          </div>
           <div class="col-md-7">
             <div class="story">
-              <h1>Our Story</h1>
+              <h1 class="display-3">Our Story</h1>
               <hr>
-              <pre>We aspire to make home gardening a common practice in our country</pre>
+              <h4 class = "my-font">Home gardening has become very popular during the last few months. People had started to grow various kinds of vegetables, fruits, and many more agricultural goods on a small scale in their own gardens. But still, there is no way for them to distribute these goods properly. And finding a buyer has been challenging. Therefore the gardeners get excessive amounts of produce piled in their homes while others who need these produce have no way of getting them.
+</h4>
             </div>  
-          </div>
-          <div class="col-md-5 pl-0 pl-md-5 center">
-            <img src="../assets/images/Mango.jpg" alt="Mango Tree" class="rounded-circle">  
           </div>
           <div class="row form-group">
           </div>
@@ -39,17 +40,26 @@
           </div>      
         </div>
         <div class="row">
-          <div class="col-md-5">
-            <b-card style="border-radius:7px;">
-              <img src="../assets/images/Mango.jpg" alt="Mango Tree" class="rounded">
-            </b-card>
-          </div>
           <div class="col-md-7 pl-0 pl-md-5">
             <div class="team">
-              <h1>Our Team</h1>
+              <h1 class="display-2">Our Team</h1>
               <hr>
-              <pre>We aspire to make home gardening a common practice in our country</pre>
+              <h4 class = "text-center">We are a dedicated team of undergraduates looking for a way to enhance the quality of life in our community.</h4>
+              <br><br>
+              <figure class="my-font text-center">
+                <blockquote class="blockquote">
+                  <h4>"The love of gardening is a seed once sawn that never dies."</h4>
+                </blockquote>
+                <figcaption class="blockquote-footer">
+                  <cite title="Source Title">Gertrude Jekyll</cite>
+                </figcaption>
+              </figure>
             </div>  
+          </div>
+          <div class="col-md-5">
+            <b-card style="border-radius:7px;">
+              <img src="../assets/images/team.jpeg" alt="Team Work" class="rounded">
+            </b-card>
           </div>
           <div class="row form-group">
           </div>    
@@ -83,5 +93,19 @@ export default {
 </script>
 
 <style scoped>
+/* Global css */
+@import url("../assets/css/styles.css");
 
+.my-font{
+    font-family: 'Indie Flower', cursive;
+    text-align: center;
+} 
+
+.form-group {
+    margin-bottom: 50px;
+}
+
+h1{
+  font-family: 'Alex Brush', cursive;
+}
 </style>
