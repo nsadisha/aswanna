7e3121e57c95f2e44c3cb7fdd805a89a1caf2ba6
nsadisha <nsadisha@gmail.com>
2021-03-23 19:01:57 +0530

    vueCookies configured

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueCookies from 'vue-cookies'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
 // Import Bootstrap an BootstrapVue CSS files (order is important)
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
+// Use vue cookies
+Vue.use(VueCookies)
+Vue.$cookies.config('5d','','',true)
 // Make BootstrapVue available throughout your project
 Vue.use(BootstrapVue)
 // Optionally install the BootstrapVue icon components plugin
